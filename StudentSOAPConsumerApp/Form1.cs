017a761302a2e3b301faaad11352da7305cd254d
ystvan <istvan.marki@gmail.com>
2016-11-07 20:06:52 +0100

    minor fixes

--- StudentSOAPConsumerApp/Form1.cs
+++ StudentSOAPConsumerApp/Form1.cs
@@ -24,6 +24,7 @@ namespace StudentSOAPConsumerApp
         public Form1()
         {
             InitializeComponent();
+            studentListView.DataSource = studentClient.GetAllStudents();
         }
 
         private void cprTextBox_TextChanged(object sender, EventArgs e)
